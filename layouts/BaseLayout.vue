d28990e5ce41f9752af0d1996c679e5b93d02ec3
Robin de Laater <robin.delaater@shopmonkey.nl>
2025-08-15 16:11:08 +0200

    Add google tag

--- layouts/BaseLayout.vue
+++ layouts/BaseLayout.vue
@@ -30,6 +30,17 @@ gsap.registerPlugin(ScrollTrigger, SplitText, TextPlugin, Draggable);
       <meta name="apple-mobile-web-app-title" content="ClickTok">
       <link rel="manifest" href="/site.webmanifest">
     </Head>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-810CQ0CV06"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-810CQ0CV06');
+    </script>
+
     <slot />
   </div>
 </template>
